c04dec2ce3c79cc13e4c3ac266948258d630cd98
Mark Chavez <markchav3z@gmail.com>
2014-09-29 17:35:50 +0800

    test to get credit balance

--- spec/lib/sms160_spec.rb
+++ spec/lib/sms160_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "net/http"
 
 describe Sms160 do
   describe "accessors" do
@@ -22,5 +23,13 @@ describe Sms160 do
       expect(sms.username).to eq "mark"
       expect(sms.password).to eq "12345"
     end
+  end
+
+  it "gets the credit balance" do
+    stub_request(:get, "#{Sms160::BALANCE_ENDPOINT}?username=username&password=password").
+    to_return(:status => 200, :body => "<?xml version='1.0' encoding='utf-8'?><string xmlns='www.160.com.au/api'>4621.00</string>", :headers => {})
+    
+    sms = Sms160::Message.new(username: "username", password: "password")
+    expect(sms.credit_balance).to eq "4621.00"
   end
 end
