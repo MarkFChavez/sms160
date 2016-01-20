d80413fceadb8305b9efe49f64bdc03a50ef4a43
Manu S Ajith <neo@codingarena.in>
2016-01-20 18:35:30 +0530

    Refactor: code to follow ruby style guide.
    Prefer single quotes over double quotes when not using string
    interpolation.
    Remove usage of redundant self.
    Code formatting.
    Freeze strings where it should not be altered with.
    Remove trailing white spaces.

--- spec/lib/sms160_spec.rb
+++ spec/lib/sms160_spec.rb
@@ -1,38 +1,38 @@
-require "spec_helper"
-require "net/http"
+require 'spec_helper'
+require 'net/http'
 
 describe Sms160 do
-  describe "accessors" do
-    it "can set the recipient of the message" do
-      sms = Sms160::Message.new(to: "+639182390239")
-      expect(sms.to).to eq "+639182390239"
+  describe 'accessors' do
+    it 'can set the recipient of the message' do
+      sms = Sms160::Message.new(to: '+639182390239')
+      expect(sms.to).to eq '+639182390239'
     end
 
-    it "can set the body of the message" do
-      sms = Sms160::Message.new(body: "hello world!")
-      expect(sms.body).to eq "hello world!"
+    it 'can set the body of the message' do
+      sms = Sms160::Message.new(body: 'hello world!')
+      expect(sms.body).to eq 'hello world!'
     end
 
-    it "can set the reply to of the message" do
-      sms = Sms160::Message.new(reply_to: "+639182390239")
-      expect(sms.reply_to).to eq "+639182390239"
+    it 'can set the reply to of the message' do
+      sms = Sms160::Message.new(reply_to: '+639182390239')
+      expect(sms.reply_to).to eq '+639182390239'
     end
   end
 
-  describe "configuration block" do
+  describe 'configuration block' do
     before(:each) do
       Sms160.configure do |config|
-        config.username = "username"
-        config.password = "password"
+        config.username = 'username'
+        config.password = 'password'
       end
     end
 
-    it "gets the credit balance" do
-      stub_request(:get, "#{Sms160::BALANCE_ENDPOINT}?username=username&password=password").
-      to_return(:status => 200, :body => "<?xml version='1.0' encoding='utf-8'?><string xmlns='www.160.com.au/api'>4621.00</string>", :headers => {})
-      
+    it 'gets the credit balance' do
+      stub_request(:get, "#{Sms160::BALANCE_ENDPOINT}?username=username&password=password")
+        .to_return(status: 200, body: "<?xml version='1.0' encoding='utf-8'?><string xmlns='www.160.com.au/api'>4621.00</string>", headers: {})
+
       sms = Sms160::Message.new
-      expect(sms.credit_balance).to eq "4621.00"
+      expect(sms.credit_balance).to eq '4621.00'
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
-require "bundler/setup"
-require "webmock/rspec"
+require 'bundler/setup'
+require 'webmock/rspec'
 
 Bundler.setup
 WebMock.disable_net_connect!(allow_localhost: true)
 
-require "sms160"
+require 'sms160'
 
 RSpec.configure do |config|
-
 end
